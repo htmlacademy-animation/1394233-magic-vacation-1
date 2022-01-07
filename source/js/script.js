f4370dc1a86863d204b6aa61382c93967ca03266
Артем Вафин <a.vafin85@gmail.com>
2022-01-07 13:21:33 +0500

    анимация шапки и подвала

--- source/js/script.js
+++ source/js/script.js
@@ -8,8 +8,10 @@ import result from './modules/result.js';
 import form from './modules/form.js';
 import social from './modules/social.js';
 import FullPageScroll from './modules/full-page-scroll';
+import pageLoaded from './modules/page-loaded.js';
 
 // init modules
+pageLoaded();
 mobileHeight();
 slider();
 menu();
