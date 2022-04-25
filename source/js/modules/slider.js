83377b4d21abbc9d7fc699cad15a26f326fc754b
Vafin Artem <a.vafin85@gmail.com>
2022-04-25 23:39:01 +0500

    Анимирует смену темы в главах Истории

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,29 +1,68 @@
 import Swiper from "swiper";
 
+const MenuButtonClass = {
+  0: `first-slide`,
+  2: `second-slide`,
+  4: `third-slide`,
+  6: `fourth-slide`,
+};
+
+const SLIDER_INIT_CLASS = `slider-init`;
+
 export default () => {
   let storySlider;
+  let currentMenuButtonClass;
+  let isSliderInit = true;
   let sliderContainer = document.getElementById(`story`);
   sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
 
+  const chageColorMenuButton = (slideIndex) => {
+    if (currentMenuButtonClass) {
+      document.body.classList.remove(currentMenuButtonClass);
+    }
+
+    currentMenuButtonClass = MenuButtonClass[slideIndex];
+    document.body.classList.add(currentMenuButtonClass);
+
+    if (isSliderInit) {
+      setTimeout(() => {
+        document.body.classList.remove(SLIDER_INIT_CLASS);
+        isSliderInit = false;
+      }, 1000);
+    }
+  };
+
   const setSlider = function () {
-    if (((window.innerWidth / window.innerHeight) < 1) || window.innerWidth < 769) {
+    if (window.innerWidth / window.innerHeight < 1 || window.innerWidth < 769) {
       storySlider = new Swiper(`.js-slider`, {
         pagination: {
           el: `.swiper-pagination`,
-          type: `bullets`
+          type: `bullets`,
         },
         keyboard: {
-          enabled: true
+          enabled: true,
         },
         on: {
           slideChange: () => {
-            if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
+            if (
+              storySlider.activeIndex === 0 ||
+              storySlider.activeIndex === 1
+            ) {
               sliderContainer.style.backgroundImage = `url("img/slide1.jpg"), linear-gradient(180deg, rgba(83, 65, 118, 0) 0%, #523E75 16.85%)`;
-            } else if (storySlider.activeIndex === 2 || storySlider.activeIndex === 3) {
+            } else if (
+              storySlider.activeIndex === 2 ||
+              storySlider.activeIndex === 3
+            ) {
               sliderContainer.style.backgroundImage = `url("img/slide2.jpg"), linear-gradient(180deg, rgba(45, 54, 179, 0) 0%, #2A34B0 16.85%)`;
-            } else if (storySlider.activeIndex === 4 || storySlider.activeIndex === 5) {
+            } else if (
+              storySlider.activeIndex === 4 ||
+              storySlider.activeIndex === 5
+            ) {
               sliderContainer.style.backgroundImage = `url("img/slide3.jpg"), linear-gradient(180deg, rgba(92, 138, 198, 0) 0%, #5183C4 16.85%)`;
-            } else if (storySlider.activeIndex === 6 || storySlider.activeIndex === 7) {
+            } else if (
+              storySlider.activeIndex === 6 ||
+              storySlider.activeIndex === 7
+            ) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg"), linear-gradient(180deg, rgba(45, 39, 63, 0) 0%, #2F2A42 16.85%)`;
             }
           },
@@ -32,7 +71,7 @@ export default () => {
           }
         },
         observer: true,
-        observeParents: true
+        observeParents: true,
       });
     } else {
       storySlider = new Swiper(`.js-slider`, {
@@ -40,14 +79,14 @@ export default () => {
         slidesPerGroup: 2,
         pagination: {
           el: `.swiper-pagination`,
-          type: `fraction`
+          type: `fraction`,
         },
         navigation: {
           nextEl: `.js-control-next`,
           prevEl: `.js-control-prev`,
         },
         keyboard: {
-          enabled: true
+          enabled: true,
         },
         on: {
           slideChange: () => {
@@ -60,14 +99,34 @@ export default () => {
             } else if (storySlider.activeIndex === 6) {
               sliderContainer.style.backgroundImage = `url("img/slide4.jpg")`;
             }
+            chageColorMenuButton(storySlider.activeIndex);
           },
           resize: () => {
             storySlider.update();
-          }
+          },
         },
         observer: true,
-        observeParents: true
+        observeParents: true,
       });
+    }
+  };
+
+  const onBodyScreenChanged = (evt) => {
+    if (evt.detail.screenName === `story`) {
+      if (isSliderInit) {
+        document.body.classList.add(SLIDER_INIT_CLASS);
+      }
+
+      if (storySlider) {
+        chageColorMenuButton(storySlider.activeIndex);
+      }
+    } else {
+      document.body.classList.remove(currentMenuButtonClass);
+
+      if (!isSliderInit) {
+        document.body.classList.remove(SLIDER_INIT_CLASS);
+        isSliderInit = true;
+      }
     }
   };
 
@@ -79,4 +138,6 @@ export default () => {
   });
 
   setSlider();
+
+  document.body.addEventListener(`screenChanged`, onBodyScreenChanged);
 };
